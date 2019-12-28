de6306c0053d4d8849719249a5de08e166ef020f
wandzi <wandzel.mateusz@yahoo.com>
2019-12-28 17:00:32 +0100

    Updates

---
+++ precache-manifest.434cc6cd4016eeb917274c524e60f14a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74400263f51291a26368668ac6e5c78",
+    "url": "/company-table/index.html"
+  },
+  {
+    "revision": "b1454e8638f18054231d",
+    "url": "/company-table/static/css/main.216f3daf.chunk.css"
+  },
+  {
+    "revision": "94db77e1f245cbc84aad",
+    "url": "/company-table/static/js/2.798d457d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/company-table/static/js/2.798d457d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1454e8638f18054231d",
+    "url": "/company-table/static/js/main.163eb432.chunk.js"
+  },
+  {
+    "revision": "f01b0d0e9d09d7aa8522",
+    "url": "/company-table/static/js/runtime-main.c9f1e15f.js"
+  }
+]);
